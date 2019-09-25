e8f34eb36d5703482668890e91eee1e5ca3c2605
Travis Johnston <travis@marketgeni.us>
2019-09-25 13:33:12 -0700

    Added stage.sh and tested hype.sh and stage.sh.
    Not fully tested.

--- hype.sh
+++ hype.sh
@@ -31,7 +31,9 @@
     repo='';
     ddest='';
     tdest='';
-    while getopts ':i:n:p:d:t:f:r:' opt; do
+    hype_opts='';
+    while getopts ':[i]:[n]:[p]:d:t:f:r:' opt; do
+        hype_opts="$hype_opts$opt";
         if [ $opt = 'i' ]; then
             i=true;
             any=true;
@@ -59,21 +61,21 @@
     done
     
     valid_args=false;
-    if [ "$f" = false ] && [ "$r" = false ]; then
-        echo "Error: a file path or repo path must be supplied to -f or -r option.";
+    if [ "$f" = false ] && [ "$r" = false ] && [ "$x" = false ]; then
+        echo "hype.sh error: a file path or repo path must be supplied to -f or -r option.";
     elif [ "$d" = true ] && [ -z "$ddest" ] && [ "$r" = false ]; then
-        echo "Error: destination file path arg missing in -d option.";
+        echo "hype.sh error: destination file path arg missing in -d option.";
     elif [ "$t" = true ] && [ -z "$tdest" ] && [ "$r" = false ]; then
-        echo "Error: destination file path arg missing in -t option.";
-    elif [ "$any" = false ]; then
-        echo "Error: -npdta option(s) must be supplied.";
-    elif [ "$f" = true ] && ![ -f $file ]; then
-        echo "Error: -f arg file path does not exist.";
+        echo "hype.sh error: destination file path arg missing in -t option.";
+    elif [ $any = false ]; then
+        echo "hype.sh error: -npdta option(s) must be supplied.";
+    elif [ "$f" = true ] && [ ! -f $file ]; then
+        echo "hype.sh error: -f arg file path does not exist.";
     elif [ "$i" = true ]; then
-        echo "Installing hype...";
+        echo "hype.sh: installing hype...";
         valid_args=true;
     else
-        echo "Begin processing...";
+        echo "hype.sh: begin processing...";
         valid_args=true;
     fi
     if [ "$valid_args" = true ]; then
@@ -83,27 +85,27 @@
             ini='/usr/local/etc/php/7.2/php.ini';
             if [ -z "$( command -v composer )" ]; then
                 valid_deps=false;
-                echo "Error: composer is required to install hype.";
+                echo "hype.sh error: composer is required to install hype.";
             fi
             if [ -z "$( php -v | grep -E '7.2' )" ]; then
                 valid_deps=false;
-                echo "Error: PHP 7.2 is required to install hype.";
+                echo "hype.sh error: PHP 7.2 is required to install hype.";
             fi
             if [ -z "$( command -v npm )" ]; then
                 valid_deps=false;
-                echo "Error: npm is required to install hype.";
+                echo "hype.sh error: npm is required to install hype.";
             fi
             if [ -z "$( command -v gem )" ]; then
                 valid_deps=false;
-                echo "Error: gem is required to install hype.";
+                echo "hype.sh error: gem is required to install hype.";
             fi
-            if ![ -f $ini ]; then
+            if [ ! -f $ini ]; then
                 valid_deps=false;
-                echo "Error: php 7.2 must be installed with homebrew.";
+                echo "hype.sh error: php 7.2 must be installed with homebrew.";
             fi
             if [ "$valid_deps" = true ]; then
                 # install phan.
-                echo "Installing phan...";
+                echo "hype.sh: installing phan...";
                 # install php-ast extension.
                 if [ -z "$( cat $ini | grep -E 'extension=ast.so' )" ]; then
                     git clone git@github.com:nikic/php-ast.git;
@@ -112,27 +114,25 @@
                     ./configure --enable-ast;
                     make install;
                     cd .. && rm -rf php-ast;
-                    # add php-ast extension to php.ini.
-                    ./php_ini_fig.sh -f $ini -i 'extension=ast.so';
                 fi
                 if [ -z "$( echo $PATH | grep -E '.composer/vendor/bin' )" ]; then
-                    echo "Enter the absolute path to your bash profile file.";
+                    echo "hype.sh: enter the absolute path to your bash profile file.";
                     profile='';
                     while -z "$profile"; do
                         read REPLY;
                         profile=$( echo $REPLY );
                         if [ -z "$profile" ]; then
                             profile='';
-                            echo "Error: please try again.";
-                        elif ![ -f $profile ]; then
+                            echo "hype.sh error: please try again.";
+                        elif [ ! -f $profile ]; then
                             profile='';
-                            echo "Error: unable to find profile. Please try agian.";
+                            echo "hype.sh error: unable to find profile. Please try agian.";
                         else
                             break;
                         fi
                     done
                     # add composer global bin to PATH.
-                    echo 'export PATH="~/.composer/vendor/bin:$PATH"' >> $profile;
+                    echo 'export PATH="'$HOME'/.composer/vendor/bin:$PATH"' >> $profile;
                 fi
                 if [ -z "$( command -v phan )" ]; then
                     # install phan composer package.
@@ -142,80 +142,99 @@
                     curl 'https://raw.githubusercontent.com/phan/phan/master/.phan/config.php' > ~/.phan/config.php;
                 fi
                 # install prettier.
-                echo "Installing prettier...";
+                echo "hype.sh: installing prettier...";
                 if [ -z "$( npm list -g --depth 0 | grep -E 'prettier' )" ]; then
                     npm install --global prettier @prettier/plugin-php;
                 fi
                 # install phrocco.
-                echo "Installing phrocco...";
+                echo "hype.sh: installing phrocco...";
                 if [ -z "$( composer global show | grep -E 'phrocco' )" ]; then
                     composer global require rossriley/phrocco:dev-master;
                 fi
                 # make .hype project.
-                mkdir -p ~/.hype;
+                echo "hype.sh: cloning hype repo...";
+                git clone https://github.com/fyfinc/hype.git;
+                mv hype ~/.hype;
                 echo '#!/bin/bash' > ~/.hype/config.sh;
+                # change exec modes.
+                chmod +x ~/.hype/hype.sh;
+                chmod +x ~/.hype/stage.sh;
+                chmod +x ~/.hype/php_ini_fig.sh;
                 chmod +x ~/.hype/config.sh;
-                echo "Finished installing hype. Hype config file at ~/.hype/config.sh.";
+                # add php-ast extension to php.ini.
+                ~/.hype/php_ini_fig.sh -f $ini -i 'extension=ast.so';
+                echo "hype.sh: finished installing hype.";
+                echo "hype.sh: hype config file at ~/.hype/config.sh.";
             fi
         else
             if [ -f ~/.hype/config.sh ]; then
+                export HYPE_OPTS=$hype_opts;
+                export HYPE_REPO=$repo;
                 if [ "$r" = true ]; then
                     # run config script.
                     source ~/.hype/config.sh;
-                else
+                    source ~/.hype/stage.sh;
+                elif [ "$r" = false ]; then
                     # run manual command.
                     if [ "$n" = true ] && [ "$f" = true ]; then
                         echo "Analyzing $file...";
                         phan -f $file;
                     fi
-                    # prettify file.
-                    if [ "$p" = true ] && [ "$f" = true ]; then
-                        echo "Prettifying $file...";
-                        prettier $file --write;
-                    fi
-                    # document file.
-                    if [ "$d" = true ] && [ "$f" = true ]; then
-                        echo "Documenting $file...";
-                        tmpdir=~/.hype/copies/$( md5 -q $file );
-                        rm -rf $tmpdir && mkdir -p $tmpdir;
-                        tmpfile=$tmpdir/$( basename $file );
-                        cp -f $file $tmpfile;
-                        phrocco -i $tmpdir -o $ddest;
-                        rm -rf $tmpdir;
-                    fi
-                    # unit test file.
-                    if [ "$t" = true ] && [ "$f" = true ]; then
-                        echo "Unit testing $file...";
-                        tmpdir=~/.hype/tests/$( md5 -q $file );
-                        rm -rf $tmpdir && mkdir -p $tmpdir;
-                        units=$( ls $tdest );
-                        for unit in $units; do
-                            echo "Running $unit...";
-                            php $tdest/$unit > $tmpdir/$unit;
-                            echo "Save results (yes/no/show)?";
-                            reply='';
-                            while -z "$reply"; do
-                                read REPLY;
-                                reply=$( echo $REPLY );
-                                if [ "$reply" = "yes" ]; then
-                                    echo "Test log saved at $tmpdir/$unit.";
-                                elif [ "$reply" = "no" ]; then
-                                    rm $tmpdir/$unit;
-                                    echo "$tmpdir/$unit log deleted.";
-                                elif [ "$reply" = "show" ]; then
-                                    echo "Showing log $tmpdir/$unit.";
-                                    cat $tmpdir/$unit;
-                                    reply='';
-                                else
-                                    echo "Please try again.";
-                                    reply='';
-                                fi
+                    echo "hype: continue (y/n)?";
+                    read REPLY;
+                    reply=$( echo $REPLY );
+                    if [ "$reply" = "y" ]; then
+                        # prettify file.
+                        if [ "$p" = true ] && [ "$f" = true ]; then
+                            echo "Prettifying $file...";
+                            prettier $file --write;
+                        fi
+                        # document file.
+                        if [ "$d" = true ] && [ "$f" = true ]; then
+                            echo "Documenting $file...";
+                            tmpdir=~/.hype/copies/$( md5 -q $file );
+                            rm -rf $tmpdir && mkdir -p $tmpdir;
+                            tmpfile=$tmpdir/$( basename $file );
+                            cp -f $file $tmpfile;
+                            phrocco -i $tmpdir -o $ddest;
+                            rm -rf $tmpdir;
+                        fi
+                        # unit test file.
+                        if [ "$t" = true ] && [ "$f" = true ]; then
+                            echo "Unit testing $file...";
+                            tmpdir=~/.hype/tests/$( md5 -q $file );
+                            rm -rf $tmpdir && mkdir -p $tmpdir;
+                            units=$( ls $tdest );
+                            for unit in $units; do
+                                echo "Running $unit...";
+                                php $tdest/$unit > $tmpdir/$unit;
+                                echo "Save results (yes/no/show)?";
+                                reply='';
+                                while -z "$reply"; do
+                                    read REPLY;
+                                    reply=$( echo $REPLY );
+                                    if [ "$reply" = "yes" ]; then
+                                        echo "Test log saved at $tmpdir/$unit.";
+                                    elif [ "$reply" = "no" ]; then
+                                        rm $tmpdir/$unit;
+                                        echo "$tmpdir/$unit log deleted.";
+                                    elif [ "$reply" = "show" ]; then
+                                        echo "Showing log $tmpdir/$unit.";
+                                        cat $tmpdir/$unit;
+                                        reply='';
+                                    else
+                                        echo "Please try again.";
+                                        reply='';
+                                    fi
+                                done
                             done
-                        done
+                        fi
+                    else
+                        echo "hype.sh: aborting...";
                     fi
                 fi
             else
-                echo "Error: you have not installed hype yet. Please use -i option.";
+                echo "hype.sh error: you have not installed hype yet. Please use -i option.";
             fi
         fi
     fi
---
+++ stage.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+if [ ! -z ${HYPE_REPOS:+${HYPE_REPOS[@]}} ] && [ ! -z ${HYPE_REPO:+$HYPE_REPO} ] && [ ! -z ${HYPE_REPOS_DOC_PATHS:+${HYPE_REPOS_DOC_PATHS[@]}} ] && [ ! -z ${HYPE_REPOS_TEST_PATHS:+${HYPE_REPOS_TEST_PATHS[@]}} ]; then
+    # find parallel array index.
+    ri=-1;
+    found=false;
+    for e in ${HYPE_REPOS[@]}; do
+        echo "passs";
+        ri=$(( $ri + 1 ));
+        e=$( basename $e );
+        if [ "$e" = "$HYPE_REPO" ]; then
+            found=true;
+            break;
+        fi
+    done
+    echo $ri;
+    if [ "$found" = true ]; then
+        if [ ! -z "$HYPE_OPTS" ]; then
+            # process repo.
+            rp=${HYPE_REPOS[$ri]};
+            docs_op=${HYPE_REPOS_DOC_PATHS[$ri]};
+            tests_op=${HYPE_REPOS_TEST_PATHS[$ri]};
+            shd=~/.hype/stage.txt;
+            cd $rp;
+            git diff --name-only --cached > $shd;
+            files=$( sed 's/^.\{1,\} \([A-Za-z0-9\-\.]\{1,\}\.php\)$/\1/p' < $shd);
+            # lint files.
+            if [ ! -z "$( echo $HYPE_OPTS ) | grep -qE 'n')" ]; then
+                echo "hype.sh: linting files...";
+                for file in $files; do
+                    ~/.hype/hype.sh -n -f $file;
+                done
+            fi
+            # auto format files.
+            if [ ! -z "$( echo $HYPE_OPTS | grep -qE 'p')" ]; then
+                echo "hype.sh: formatting files...";
+                for file in $files; do
+                    ~/.hype/hype.sh -p -f $file;
+                done
+            fi
+            # document files.
+            if [ ! -z "$( echo $HYPE_OPTS | grep -qE 'd' )" ]; then
+                echo "hype.sh: documenting files...";
+                for file in $files; do
+                    ~/.hype/hype.sh -d $docs_op -f $file;
+                done;
+            fi
+            # unit test files.
+            if [ ! -z "$( echo $HYPE_OPTS | grep -qE 't' )" ]; then
+                echo "hype.sh: unit testing files...";
+                for file in $files; do
+                    ~/.hype/hype.sh -t $tests_op -f $file;
+                done;
+            fi
+            rm $shd;
+        else
+            echo "hype.sh error: missing options/args.";
+        fi
+    else
+        echo "hype.sh error: no repo found in config file.";
+    fi
+else
+    echo "hype.sh error: missing repo some or all config info.";
+fi
